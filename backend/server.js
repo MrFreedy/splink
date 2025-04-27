6300352aef42bf19fb1aa68adb98a8b8847ac89b
MrFreedyTLS <paul.noguez.vlife@gmail.com>
2025-04-27 21:02:07 +0200

    add CORS to API

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3000;
 
 const userRouter = require('./routes/users');
 
+app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 app.use('/users', userRouter);
