786b8b358673e02063c3e1744df34d36f323f9ef
MrFreedy <arthur.lenne21@gmail.com>
2025-04-29 00:20:15 +0200

    create an API Service and refactor Login Component

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { RouterModule, Router } from '@angular/router';
 import { catchError, tap } from 'rxjs/operators';
 import { of } from 'rxjs';
 import { FormsModule } from '@angular/forms';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-login',
@@ -15,26 +15,21 @@ export class LoginComponent {
   email: string = '';
   password: string = '';
 
-  constructor(private http: HttpClient, private router: Router) { }
+  constructor(private apiService: ApiService, private router: Router) {}
 
   login() {
-    this.http.post<{ message: string, user: { _id: string, email: string, username: string, password: string } }>('http://217.65.146.195:3000/users/login', {
-      email: this.email,
-      password: this.password
-    })
-    .pipe(
-      tap(response => {
-        const { password, ...user } = response.user;
-        localStorage.setItem('user', JSON.stringify(user));
-        console.log('User data saved to localStorage', user);
-        this.router.navigate(['/dashboard']);
-      }),
-      catchError(error => {
-        console.error('Login failed', error);
-        alert('Login failed. Please check your credentials.');
-        return of(null);
-      })
-    )
-    .subscribe();
+    this.apiService.login(this.email, this.password)
+      .pipe(
+        tap((response: { message: string, user: { _id: string, email: string, username: string, password: string } }) => {
+          const { password, ...user } = response.user;
+          localStorage.setItem('user', JSON.stringify(user));
+          this.router.navigate(['/dashboard']);
+        }),
+        catchError(error => {
+          alert('Login failed. Please check your credentials.');
+          return of(null);
+        })
+      )
+      .subscribe();
   }
 }
