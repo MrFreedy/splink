6decfc6cefd79b41c5abf3fefe5277a702ee6e7a
MrFreedyTLS <paul.noguez.vlife@gmail.com>
2025-05-02 17:27:39 +0200

    force slider to climb up when page changes

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterOutlet, RouterModule } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-
+import { RouterOutlet, Router, RouterModule, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterModule, LoginComponent],
+  imports: [RouterOutlet, RouterModule],
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'splink';
+  constructor(private router: Router) {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      window.scrollTo(0, 0);
+    });
+  }
 }
