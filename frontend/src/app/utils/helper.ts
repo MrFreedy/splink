2b2a511d99acc4420f0077713e79090370b871f8
MrFreedyTLS <paul.noguez.vlife@gmail.com>
2025-05-08 16:36:37 +0200

    update depense and evenement categories

--- frontend/src/app/utils/helper.ts
+++ frontend/src/app/utils/helper.ts
@@ -6,12 +6,22 @@ import { Injectable } from '@angular/core';
 export class Helper {
   getIcon(category: string): string {
     switch (category.toLowerCase()) {
-      case 'courses': return 'icons/grocery/grocery-normal.png';
-      case 'loyer': return 'icons/loan/loan-normal.png';
-      case 'électricité': return 'icons/electricity/electricity-normal.png';
-      case 'covoiturage': return 'icons/covoiturage/covoiturage-normal.png';
-      case 'poubelles': return 'icons/trash/trash-normal.png';
-      default: return 'icons/default.png';
+      case 'abonnement': return 'icons/subscription/subscription-hover.png';
+      case 'covoiturage': return 'icons/covoiturage/covoiturage-hover.png';
+      case 'course': return 'icons/grocery/grocery-hover.png';
+      case 'eau': return 'icons/water/water-hover.png';
+      case 'electricite': return 'icons/electricity/electricity-hover.png';
+      case 'fete': return 'icons/party/party-hover.png';
+      case 'gaz': return 'icons/gas/gas-hover.png';
+      case 'internet': return 'icons/internet/internet-hover.png';
+      case 'loyer': return 'icons/loan/loan-hover.png';
+      case 'loyer': return 'icons/loan/loan-hover.png';
+      case 'nettoyage': return 'icons/cleaning/cleaning-hover.png';
+      case 'poubelles': return 'icons/trash/trash-hover.png';
+      case 'reparation': return 'icons/reparation/reparation-hover.png';
+      case 'restaurant': return 'icons/restaurant/restaurant-hover.png';
+      case 'autre': return 'icons/more/more-hover.png';
+      default: return 'icons/more/more-hover.png';
     }
   }
 
