0bffd602b5bb0547144a3b087f5e5bb5bd173e08
MrFreedyTLS <paul.noguez.vlife@gmail.com>
2025-05-08 17:43:10 +0200

    fix icon style

--- frontend/src/app/utils/helper.ts
+++ frontend/src/app/utils/helper.ts
@@ -6,19 +6,19 @@ import { Injectable } from '@angular/core';
 export class Helper {
   getIcon(category: string): string {
     switch (category.toLowerCase()) {
-      case 'abonnement': return 'icons/subscription/subscription-hover.png';
-      case 'covoiturage': return 'icons/covoiturage/covoiturage-hover.png';
-      case 'course': return 'icons/grocery/grocery-hover.png';
-      case 'eau': return 'icons/water/water-hover.png';
-      case 'electricite': return 'icons/electricity/electricity-hover.png';
-      case 'fete': return 'icons/party/party-hover.png';
-      case 'gaz': return 'icons/gas/gas-hover.png';
-      case 'internet': return 'icons/internet/internet-hover.png';
-      case 'loyer': return 'icons/loan/loan-hover.png';
-      case 'nettoyage': return 'icons/cleaning/cleaning-hover.png';
-      case 'poubelles': return 'icons/trash/trash-hover.png';
-      case 'reparation': return 'icons/reparation/reparation-hover.png';
-      case 'restaurant': return 'icons/restaurant/restaurant-hover.png';
+      case 'abonnement': return 'icons/subscription/subscription-normal.png';
+      case 'covoiturage': return 'icons/covoiturage/covoiturage-normal.png';
+      case 'course': return 'icons/grocery/grocery-normal.png';
+      case 'eau': return 'icons/water/water-normal.png';
+      case 'electricite': return 'icons/electricity/electricity-normal.png';
+      case 'fete': return 'icons/party/party-normal.png';
+      case 'gaz': return 'icons/gas/gas-normal.png';
+      case 'internet': return 'icons/internet/internet-normal.png';
+      case 'loyer': return 'icons/loan/loan-normal.png';
+      case 'nettoyage': return 'icons/cleaning/cleaning-normal.png';
+      case 'poubelles': return 'icons/trash/trash-normal.png';
+      case 'reparation': return 'icons/reparation/reparation-normal.png';
+      case 'restaurant': return 'icons/restaurant/restaurant-normal.png';
       case 'autre': return 'icons/more/more-hover.png';
       default: return 'icons/more/more-hover.png';
     }
